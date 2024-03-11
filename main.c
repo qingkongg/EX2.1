a1527e7590a33f083f95408ffad8fae2144cf887
qingkongg <2241966271@qq.com>
2024-03-11 01:54:11 -0700

    ex2quuestion2

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 int main(void) {
   Vector *vector = vector_create();
   if(vector == NULL){
-    free(vector);
     return -1;
   }
   for (int i = 0; i < 100; ++i) {
     vector_push(vector, i * 10 / 7.0);
+    if(vector == NULL){
+      return -1;
+    }
   }
   for (int i = 0; i < 100; ++i) {
     printf("%f\n", vector_get(vector, i));
--- vector.c
+++ vector.c
@@ -10,24 +10,40 @@ Vector *vector_create(void) {
   vector->capacity = VECTOR_INITIAL_CAPACITY;
   vector->data = malloc(sizeof(double) * vector->capacity);
   if(vector->data == NULL){
-    free()
+    return NULL;
   }
   return vector;
 }
 
 void vector_push(Vector *vector, double element) {
+  if(vector == NULL)
+    return;
   if (vector->size == vector->capacity) {
     vector->capacity *= 2;
-    vector->data = realloc(vector->data, sizeof(double) * vector->capacity);
+    double * newdata = realloc(vector->data, sizeof(double) * vector->capacity);
+    //vector->data = realloc(vector->data, sizeof(double) * vector->capacity);
+    if(newdata){
+      vector->data = newdata;
+    }
+    else{
+      free(vector->data);
+      free(vector);
+      vector = NULL;
+    }
   }
   vector->data[vector->size++] = element;
 }
 
 double vector_get(const Vector *vector, int index) {
+  if(vector == NULL){
+    return;
+  }
   return vector->data[index];
 }
 
 void vector_free(Vector *vector) {
+  if(vector == NULL)
+    return;
   free(vector->data);
   free(vector);
 }
